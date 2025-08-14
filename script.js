7a3a7cda6b378db52bdc8c0f9abb8d1fb9590715
Okedigwe <152255378+Okedigwe@users.noreply.github.com>
2025-08-14 07:59:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// This is a basic JavaScript file.
+// You can add code here for animations, form validation, etc.
+
+console.log('Website is ready!');
+
+// Example: Simple function to handle a click event
+// const ctaButton = document.querySelector('.cta-button');
+// ctaButton.addEventListener('click', () => {
+//     alert('You clicked the CTA button!');
+// });
