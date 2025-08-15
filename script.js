4a4cc9027403d8a33c6cfc4ddec99163ded1510b
Okedigwe <152255378+Okedigwe@users.noreply.github.com>
2025-08-15 14:57:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -10,34 +10,29 @@ console.log('Website is ready!');
 // });
 
 // Get the button
-let mybutton = document.getElementById("backToTopBtn");
+//let mybutton = document.getElementById("backToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button 
-// // This is the function that shows/hides the button
-window.onscroll = function() {scrollFunction()};
-
-document.addEventListener('DOMContentLoaded'), () => {
+document.addEventListener('DOMContentLoaded', () => {
     let mybutton = document.getElementById("backToTopBtn");
-}
 
-if (mybutton) {
+    if (mybutton) {
         window.onscroll = function() {
             scrollFunction();
         };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                mybutton.classList.add("show");
+            } else {
+                mybutton.classList.remove("show");
+            }
+        }
+
+        mybutton.addEventListener("click", backToTop);
+
+        function backToTop() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        }
     }
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+});
